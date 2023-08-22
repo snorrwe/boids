79e4fc64d325ce432e0e705fb9c2efae309d9ad8
Daniel Kiss <littlesnorrboy@gmail.com>
2023-08-22 10:22:44 +0200

    Use smaller types to update boids
    Hoping to better utilize the hardware cache.
    In tests on my machine I've seen no significant difference in
    performance.

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,26 @@ use engine::{
         sprite_renderer::{self, SpriteSheet},
         GraphicsState,
     },
-    transform::{self, transform_bundle, GlobalTransform, Transform},
+    transform::{self, transform_bundle, Transform},
     App, DefaultPlugins, DeltaTime, Plugin, Stage,
 };
 use tracing::info;
 
 struct Boid;
 
-struct Velocity(pub Vec3);
-struct LastVelocity(pub Vec3);
+struct Velocity(pub Vec2);
+struct LastVelocity(pub Vec2);
+
+struct Pos(pub Vec2);
+struct LastPos(pub Vec2);
+
+impl std::ops::Deref for Pos {
+    type Target = Vec2;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 struct BoidConfig {
     radius: f32,
@@ -26,11 +37,11 @@ struct BoidConfig {
     min_vel: f32,
 }
 
-const N: usize = 1000;
+const N: usize = 10000;
 
 fn update_boids(
-    mut q: Query<(EntityId, &mut Transform, &mut Velocity, &LastVelocity), With<Boid>>,
-    positions: Query<(&GlobalTransform, EntityId), With<Boid>>,
+    mut q: Query<(EntityId, &mut Pos, &mut Velocity, &LastVelocity), With<Boid>>,
+    positions: Query<(&LastPos, EntityId), With<Boid>>,
     conf: Res<BoidConfig>,
     dt: Res<DeltaTime>,
 ) {
@@ -39,14 +50,14 @@ fn update_boids(
     let min_vel = conf.min_vel;
     let dt = dt.0.as_secs_f32();
     q.par_for_each_mut(|(id, tr, vel, last_vel)| {
-        let pos = tr.pos;
-        let mut dir = -min_vel * tr.pos.normalize_or_zero(); // move towards the center if no other
-                                                             // boids are in sight
+        let pos = tr.0;
+        let mut dir = -min_vel * pos.normalize_or_zero(); // move towards the center if no other
+                                                          // boids are in sight
         positions.iter().for_each(|(gtr, boid_id)| {
             if id == boid_id {
                 return;
             }
-            let d = pos - gtr.0.pos;
+            let d = pos - gtr.0;
             let mag = d.length();
             if mag < radius && vel.0.dot(d) < 0.0 {
                 let ratio = (mag / sepa).clamp(0.01, 1.0);
@@ -54,20 +65,27 @@ fn update_boids(
             }
         });
         vel.0 = dir.lerp(last_vel.0, 0.5);
-        tr.pos += vel.0 * dt;
+        tr.0 += vel.0 * dt;
     });
 }
 
-fn sprite_rotate(mut q: Query<(&mut Transform, &Velocity)>) {
-    q.par_for_each_mut(|(tr, Velocity(vel))| {
+fn update_transform(mut q: Query<(&mut Transform, &Velocity, &Pos)>) {
+    q.par_for_each_mut(|(tr, Velocity(vel), p)| {
         let angle = -vel.x.atan2(vel.y);
         tr.rot = Quat::from_rotation_z(angle);
+        tr.pos = p.extend(0.0);
     });
 }
 
 fn update_boids_vel(mut q: Query<(&mut LastVelocity, &Velocity)>) {
     q.par_for_each_mut(move |(l, vel)| {
         l.0 = vel.0;
+    });
+}
+
+fn update_boids_pos(mut q: Query<(&mut LastPos, &Pos)>) {
+    q.par_for_each_mut(move |(l, p)| {
+        l.0 = p.0;
     });
 }
 
@@ -113,8 +131,10 @@ fn setup_boids(
             .insert_bundle(sprite_renderer::sprite_sheet_bundle(boid.clone(), None))
             .insert_bundle((
                 Boid,
-                LastVelocity(Vec3::ZERO),
-                Velocity(Vec3::new(vx, vy, 0.0)),
+                Pos(Vec2::new(x, y)),
+                LastPos(Vec2::new(x, y)),
+                LastVelocity(Vec2::ZERO),
+                Velocity(Vec2::new(vx, vy)),
             ));
     }
 }
@@ -146,7 +166,8 @@ impl Plugin for GamePlugin {
         app.stage(Stage::Update)
             .add_system(update_boids)
             .add_system(update_boids_vel.after(update_boids))
-            .add_system(sprite_rotate.after(update_boids));
+            .add_system(update_boids_pos.after(update_boids))
+            .add_system(update_transform.after(update_boids));
 
         app.add_startup_system(setup_boids);
         app.insert_resource(BoidConfig {
